b3c168d1842b98082685e5b6137293bc81f3b32b
BRAINNET\Pieter <pieter.plomp@brainnet.nl>
2018-09-10 14:37:22 +0200

    Add project files.

---
+++ LogErrorAttribute.cs
@@ -0,0 +1,18 @@
+﻿//using System.Diagnostics;
+//using MethodBoundaryAspect.Fody.Attributes;
+
+//namespace MethodBoundaryAspect.Fody.Sample
+//{
+//    public class LogErrorAttribute : OnMethodBoundaryAspect
+//    {
+//        public override void OnException(MethodExecutionArgs args)
+//        {
+//            if (args.Exception == null)
+//            {
+//                return;
+//            }
+
+//            Debug.WriteLine(args.Exception);
+//        }
+//    }
+//}
