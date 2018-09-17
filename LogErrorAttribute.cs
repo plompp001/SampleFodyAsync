70409f75c96e814602cfdfb6df3e80f19eb86182
Pieter Plomp <pplomp84@live.nl>
2018-09-17 12:08:26 +0200

    Update LogErrorAttribute.cs

--- LogErrorAttribute.cs
+++ LogErrorAttribute.cs
@@ -1,18 +1,18 @@
-﻿//using System.Diagnostics;
-//using MethodBoundaryAspect.Fody.Attributes;
+using System.Diagnostics;
+using MethodBoundaryAspect.Fody.Attributes;
 
-//namespace MethodBoundaryAspect.Fody.Sample
-//{
-//    public class LogErrorAttribute : OnMethodBoundaryAspect
-//    {
-//        public override void OnException(MethodExecutionArgs args)
-//        {
-//            if (args.Exception == null)
-//            {
-//                return;
-//            }
+namespace MethodBoundaryAspect.Fody.Sample
+{
+    public class LogErrorAttribute : OnMethodBoundaryAspect
+    {
+        public override void OnException(MethodExecutionArgs args)
+        {
+            if (args.Exception == null)
+            {
+                return;
+            }
 
-//            Debug.WriteLine(args.Exception);
-//        }
-//    }
-//}
+            Debug.WriteLine(args.Exception);
+        }
+    }
+}
